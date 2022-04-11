c4b8cf72ec165f6167f49653fb160d7a08d63e10
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-04-11 14:36:13 -0300

    aula 5

---
+++ 3- JavaScrip_Iniciante/5-String.js
@@ -0,0 +1,11 @@
+//  indices ->   01234567
+let umaString = 'Um texto';
+
+console.log(umaString[3]);
+// vai pegar apenas o elemento do indie 3
+
+console.log(umaString.charAt(7));
+//funciona da mesma forma
+
+console.log(umaString.concat(' muito bonito'));
+//concatena (adiciona) elementos a string
