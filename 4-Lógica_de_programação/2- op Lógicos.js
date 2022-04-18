8ec03096da458770986b2bcfa70c0e8e3f158684
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-04-18 18:42:35 -0300

    OP LOGICOS

---
+++ 4-Lógica_de_programação/2- op Lógicos.js
@@ -0,0 +1,18 @@
+/*
+OPERADORES LÓGICOS
+
+&& - and - E        tudo deve ser true para retornar true
+|| - or  - ou
+!  - not - Não
+
+*/
+console.log( true && true); // true
+console.log( true && false);// false
+
+console.log( true || true);//true
+console.log( true || false);//true
+
+console.log(!false) // true
+console.log(!true) // false
+console.log(!!true) // true (duas negações retorna ao valore original)
+
