dd20e5d51806e2b41111d68c47f0f382a789d885
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-04-22 10:31:16 -0300

    alteração css e js

--- 4-Lógica_de_programação/4-Exercicio_IMC/assets/main - Copia.js
+++ 4-Lógica_de_programação/4-Exercicio_IMC/assets/main - Copia.js
@@ -1,9 +1,5 @@
 const formulario = document.querySelector('.formulario');
 const resultEscrito = document.querySelector('.resul');
-const inputAlt = formulario.querySelector('#altura');
-const inputPeso= formulario.querySelector('#peso');
-const peso = Number(inputPeso.value);
-const alt = Number(inputAlt.value);
 
 formulario.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -15,58 +11,58 @@ function mudaLinha(x) { // Usa o numero da tr como paramentro (0 a 6)
     document.getElementsByTagName('tr')[x].style.fontSize = '30px';
     document.getElementsByTagName('tr')[x].style.color = 'white';
 }
-function getIMC() {//calculo do imc;
-    let imc = (peso / Math.pow(alt, 2)).toFixed(2);//potenciação
+
+function CalculoIMC() {
+
+
+const inputAlt = formulario.querySelector('#altura');
+const inputPeso= formulario.querySelector('#peso');
+const peso = Number(inputPeso.value);
+const alt = Number(inputAlt.value);
+let imc;
+let diagnostico;
+
+//calculo do imc;
+imc = Number((peso / Math.pow(alt, 2)).toFixed(2));//potenciação
     
-    return imc;
-}
 
-function CalculoIMC(imc) {
-
-    let diagnostico;
-    
-
-
-    if (peso == undefined || peso == "" || peso == NaN) {
+    if (!peso) {
+        //peso == undefined || peso == "" || peso == NaN
 
         const inputPeso = document.querySelector('#peso');
         inputPeso.style.background = 'red';
         inputPeso.innerText = 'Digite o peso!'// não funciona
         resultEscrito.innerHTML += '<p>Erro digite um valor válido para o peso </p>';
     }
-    if (alt == undefined || alt == "" || alt == NaN) {
+    if (!alt) {
+        //alt == undefined || alt == "" || alt == NaN
+
         const inputAlt = document.querySelector('#altura');
         inputAlt.style.background = 'red';
-        inputAlt.innerHTML = 'Digite o peso!'// não funciona
+        inputAlt.innerHTML = 'Digite a altura!'// não funciona
         resultEscrito.innerHTML += '<p> Erro digite um valor válido para a altura </p>';
     }
-
-    if (imc < 18.5) {
-        mudaLinha(1)
-        diagnostico = 'Abaixo do peso';
-
-    } else if (imc >= 18.5 && imc <= 24.9) {
-        mudaLinha(2)
-        diagnostico = 'Peso Normal';
-
-    } else if (imc >= 25 && imc <= 29.9) {
-        mudaLinha(3)
-        diagnostico = 'Sobrepeso';
-
-    } else if (imc >= 30 && imc <= 34.9) {
-        mudaLinha(4)
-        diagnostico = 'Obesidade nivel 1';
-
-    } else if (imc >= 35 && imc <= 39.9) {
-        mudaLinha(5)
-        diagnostico = 'Obesidade nivel 2';
-
-    } else if (imc >= 40) {
+    
+    if (imc >= 40) { // melhorando a logica fazendo do maior para o menor
         mudaLinha(6)
         diagnostico = 'Obesidade nivel 3';
-
-    }
-
+    } else if (imc >= 35 ) {
+        mudaLinha(5)
+        diagnostico = 'Obesidade nivel 2';
+    }else if (imc >= 30 ) {
+        mudaLinha(4)
+        diagnostico = 'Obesidade nivel 1';
+    }else if (imc >= 25 && imc <= 29.9) {
+        mudaLinha(3)
+        diagnostico = 'Sobrepeso';
+    }  else if (imc >= 18.5 ) {
+        mudaLinha(2)
+        diagnostico = 'Peso Normal';
+    }else if (imc < 18.5) { 
+        mudaLinha(1)
+        diagnostico = 'Abaixo do peso';
+    }   
+  
     console.log(peso, alt, imc);
     console.log(typeof peso);
     console.log(typeof alt);
