2f0b16be9a357c6406cc9f3c2b7249197d5fcb10
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-04-26 15:09:27 -0300

    EXERCICIO 6

--- 4-Lógica_de_programação/6-Exercicio Date/assets/main.js
+++ 4-Lógica_de_programação/6-Exercicio Date/assets/main.js
@@ -3,14 +3,14 @@
 //MINHA SOLUÇÃO
 const data= new Date();
 const diaSemana = data.toLocaleString('pt-BR', {weekday: 'long'}); //função nativa para semana
-const mesLongo = data.toLocaleDateString('pt-BR',{month: 'long'});//função nativa para o mes
-const dia = data.getDay();
-let diaComZero = dia <10 ? `0${dia}` : dia ;
-const ano = data.getFullYear();
-const horaAtual =  data.toLocaleTimeString('pt-BR')
+const dataLonga = data.toLocaleDateString('pt-BR',{dateStyle: 'long'});//função nativa para a data
+const horas = data.getHours();
+const min =  data.getMinutes('2-digit');
 
 const trocaData = document.querySelector('h1');
 const trocaHora = document.querySelector('h2');
 
-trocaData.innerHTML = `${diaSemana} , ${diaComZero} de ${ mesLongo} de ${ano}`;
-trocaHora.innerHTML = `${horaAtual}`
+trocaData.innerHTML = `${diaSemana} , ${dataLonga} `;
+trocaHora.innerHTML = `${horas}:${min}`;
+
+
