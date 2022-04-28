27ba9a8f60c5a9e0452567e7ce6981633a5189f1
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-04-28 18:29:37 -0300

    For classico + for in

--- 4-Lógica_de_programação/11.FOR_in.js
+++ 4-Lógica_de_programação/11.FOR_in.js
@@ -1,5 +1,6 @@
 // FOR in => LÊ OS INDICES OU CHAVES DOS OBJETOS
 
+//Exemplo 1 com Array
 const frutas= ['maca','banana','pera','uva',''];
 
 for (let i= 0; i <frutas.length;i++){
@@ -12,3 +13,20 @@ for (let i in frutas){
     console.log(i); // lê o indice dos elementos APENAS
     console.log (frutas[i]);
 }//forin
+
+
+//Exemplo 2 com Objeto ++++++++++++++++++
+
+const pessoa = {
+    nome:'Joao',
+    sobrenome: 'Esteves',
+    idade: 38,
+    mat: 2021216861
+};
+console.log (pessoa.nome)// 1- formas comuns de acessar as chaves
+console.log (pessoa['nome']);// 2- formas comuns de acessar as chaves
+for(let chave in pessoa){
+    console.log(chave); // leu as chaves nome ,sobrenome ,idade,mat.
+    console.log (pessoa[chave]);//USANDO O FOR IN
+    //leu o conteudo da chave
+}
--- 4-Lógica_de_programação/9- Estruturas_de_repetição-FOR.js
+++ 4-Lógica_de_programação/9- Estruturas_de_repetição-FOR.js
@@ -19,7 +19,17 @@ for (let i =0  ; i <10 ; i++) {
 }
 //---------------------------------------------
 console.log(/* espaçamento */);
+//---------------------------------------------
+const seuNome = ' Joao Henrique';
+for (let i = 0; i < seuNome.length; i++) {
+    console.log(seuNome[i]);    
+} // percorre todo o indice da string, letra por letra
 
+
+
+//---------------------------------------------
+console.log(/* espaçamento */);
+//---------------------------------------------
 //percorrendo um Array
 const frutas = [ 'maca', 'banana', 'limao', 'pera'];
 console.log ('qtd de elementos',frutas.length); // =4
@@ -27,3 +37,4 @@ console.log ('qtd de elementos',frutas.length); // =4
 for (let i=0; i < frutas.length; i++){
     console.log (`Indice ${i} = ${frutas[i]}`);
 }
+//---------------------------------------------
