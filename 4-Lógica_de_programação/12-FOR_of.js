908a45ac89cc8ee01dd3c108dbfeea2ffa4fb5b3
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-05-02 10:02:13 -0300

    FOR OF

---
+++ 4-Lógica_de_programação/12-FOR_of.js
@@ -0,0 +1,29 @@
+const cab = 'aprendendo JS';
+
+for (let valor of cab){
+    console.log(valor);
+}
+/*FOR OF FAZ COM QUE O LAÇO LEIA O CONTEUDO DA STRING
+a
+p
+r
+e
+n
+d
+e
+n
+d
+o
+ 
+J
+S
+
+*/
+const nomes =['Maison','Gustavo','Lucas', 'Renato'];
+
+for (const i of nomes) {// fez a leitura dos elementos do array
+    console.log(i)      //Maison Gustavo Lucas Renato
+}
+
+//não funciona com objetos
+
