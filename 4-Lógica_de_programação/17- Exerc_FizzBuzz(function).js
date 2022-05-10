394bc0dfe4d5c4cf254cb6bf455e15cee8f48e62
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-05-10 17:01:41 -0300

    exercicio fizzbuzz

---
+++ 4-Lógica_de_programação/17- Exerc_FizzBuzz(function).js
@@ -0,0 +1,32 @@
+/**
+ * ESCREVA UMA FUNÇÃO QUE RECEBA UM NUMERO E RETORNE:
+ * DIVISIVEL POR 3 = FIZZ
+ * DIVISIVEL POR 5 = BUZZ
+ * DIVISIVEL POR 3 E 5 = FIZZBUZZ
+ * NUMERO "NÃO" DIVISIVEL POR 3 E 5 + RETORNA O PROPRIO NUMERO
+ * CHEGAR SE O NUMERO É REALMENTE UM NUMBER
+ * USAR FUNÇÃO PARA NUM DE 0 A 100 
+ */
+
+//let num = 13 //variavel de controle
+
+function isNumber(num) {
+    if (typeof num === 'number') return (FizzBuzz(num));
+
+    else return (console.log(`numero "${num}"  invalido, tente novamente`));
+};
+function FizzBuzz(num) {
+    
+    if (num %3 === 0 && num %5 === 0) return ('## Numero FizzBuzz');        
+    
+    else if (num %3 === 0) return ('# Numero Fizz');
+    
+    else if(num %5 === 0) return ('# Numero Buzz');
+
+    else  return (' não passa no teste');
+};
+
+for (let index = 0; index <= 100; index++) {
+    
+    console.log(index, isNumber(index));
+}
