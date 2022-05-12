0397ece52990485a3fa1eaf9ee57173b1abc13d5
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-05-12 18:50:03 -0300

    teste

--- 4-Lógica_de_programação/19- Erro_2-Try-Catch-Finally.js
+++ 4-Lógica_de_programação/19- Erro_2-Try-Catch-Finally.js
@@ -15,10 +15,25 @@ try {
 try {
     console.log('Abrindo o arquivo');
     console.log('Manipulando o arquivo');
-    console.log(a); // simulando erro
+    //console.log(a); // simulando erro
     console.log('Fecchando o arquivo');
 } catch (error) {
     console.log('ERRO! Tratando o erro');
 }finally{
     console.log('FINALLY: Sempre serei executado');
 }
+
+//==================================================
+console.log('');
+
+//EXERCICIO
+
+
+function mostraHora(data) {
+    if (!(data instanceof Date)) {
+        //se não for objetos de data, irá dar erro
+        throw new TypeError ('Esperando uma Instancia de Date');
+    }
+    }
+// mostraHora(new Date()); // forma certa
+mostraHora(1);
