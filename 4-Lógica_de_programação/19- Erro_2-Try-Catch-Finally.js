0f921f1ad54d8c73ee4856f6671a122ed5b06737
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-05-16 12:21:57 -0300

    try, catch, finally

--- 4-Lógica_de_programação/19- Erro_2-Try-Catch-Finally.js
+++ 4-Lógica_de_programação/19- Erro_2-Try-Catch-Finally.js
@@ -17,6 +17,8 @@ try {
     console.log('Manipulando o arquivo');
     //console.log(a); // simulando erro
     console.log('Fecchando o arquivo');
+
+
 } catch (error) {
     console.log('ERRO! Tratando o erro');
 }finally{
@@ -30,10 +32,32 @@ console.log('');
 
 
 function mostraHora(data) {
-    if (!(data instanceof Date)) {
+    
+    if (data && !(data instanceof Date)) {
         //se não for objetos de data, irá dar erro
         throw new TypeError ('Esperando uma Instancia de Date');
     }
+    if(!data){
+        data = new Date()
+        // Se não enviar uma data ele gera automaticamente
     }
-// mostraHora(new Date()); // forma certa
-mostraHora(1);
+    return data.toLocaleTimeSpring('pt-BR',
+    {hour: '2-digit',
+    minute:'2-digit',
+    second: '2-digit',
+    hour12: false
+    
+    });
+}
+
+try {
+    const data = new Date ('14-06-1984 09:00:10')
+    let hora = mostraHora(data);
+    console.log(hora);
+    
+} catch (error) {
+    console.log('Deu erro na execução');
+    console.log(error);
+}  finally{
+    console.log('____Fim da execução____');
+}  
