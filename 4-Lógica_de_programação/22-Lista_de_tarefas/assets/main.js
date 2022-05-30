a6c689d5d2d906994e2a23d3bf4211055ac03042
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-05-30 17:33:18 -0300

    exercicio

--- 4-Lógica_de_programação/22-Lista_de_tarefas/assets/main.js
+++ 4-Lógica_de_programação/22-Lista_de_tarefas/assets/main.js
@@ -1,19 +1,62 @@
-// alert('teste');
-const inputPrincipal = document.querySelector('.input-nova-tarefa');//input
+// variaveis globais
+
+const inputNovaTarefa = document.querySelector('.input-nova-tarefa');//input de texto principal
 const caixaLi = document.querySelector('.tarefas');//ul
-const adiciona = document.querySelector('.btn-adiciona');//button
+const btnAdiciona = document.querySelector('.btn-adiciona');//button
 
 
 function criaLi() {
-    let li = document.createElement('li');
-    let button = document.createElement('button');
+    //criando a li
+    const li = document.createElement('li');
+    li.classList.add('nova-tarefa');
+    return li;//retornamos o li para usar para criar o botão
+}    
 
-    caixaLi.appendChild(li);
-    li.innerHTML += 'teste';
-    caixaLi.append(button);
+function apagaInputPrincipal(){
+    inputNovaTarefa.value = '';
+    inputNovaTarefa.focus();
+    //vai fazer o cursor piscar dentro da caixa input
+    
+};
+
+function criaBotaoApagar(li){
+    //criando o botão apagar
+    li.innerText += ' ';
+      let button = document.createElement('button');
+      button.setAttribute('type', 'button');
+      button.classList.add('botao-apagar');
+      button.innerText = 'Apagar';
+      li.appendChild(button);
+
+}
+inputNovaTarefa.addEventListener('keypress', function(e) {
+     //capturando evento de teclas digitadas
+     console.log(e); // testando o evento e descobrindo o num da tecla
+    if (e.keyCode === 13/* codigo do botão enter */) {
+
+     if (!inputNovaTarefa.value) return;
+      criaTarefa(inputNovaTarefa.value);
+      // *caso a caixa não esteja vazia retornar o valor para a função criaTarefa.
+    }
+   });
+
+
+function criaTarefa(textoCapturado) {
+   const li = criaLi();
+   li.innerText= textoCapturado;
+   caixaLi.appendChild(li);
+    console.log(textoCapturado);
+    apagaInputPrincipal();
+    criaBotaoApagar(li);
 }
 
-adiciona.addEventListener('click', function (params) {
-    // alert('clicou')
-    criaLi();
+btnAdiciona.addEventListener('click', function (params) {
+    //capturando o click no button "adicionar tarefas"
+
+    // alert('clicou') //teste
+    if (!inputNovaTarefa.value) return
+    //evitar que crie li's vazias com um if simples 
+    
+    criaTarefa(inputNovaTarefa.value);
 })
+
