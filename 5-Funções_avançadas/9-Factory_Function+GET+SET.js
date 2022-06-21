ead37c6a233179b845a8dc652ceab5de0862380d
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-06-21 14:46:19 -0300

    Setter e Getter

--- 5-Funções_avançadas/9-Factory_Function+GET+SET.js
+++ 5-Funções_avançadas/9-Factory_Function+GET+SET.js
@@ -24,7 +24,16 @@ function criaPessoa(nome, sobrenome, peso, alt) {
             //GETTER: GET
         get nomeCompleto() { //GET = vai fazer o metodo(funçao) virar um atributo (string), não vai precisar dos '()'
             return `${this.nome} ${this.sobrenome}`
+        },
+
+            //SETTER: SET
+        // USADO PARA POSER ALTERAR O VALOR DO ATRIBUTO
+        set nomeCompleto(valor) {
+            valor = valor.split(' '); // adicionar espaçamento entre palavras
+            this.nome = valor.shift(); //apaga o primeiro elemento do array
+            this.sobrenome = valor.join(" "); // juntando o resto em uma string e separando com um espaço ' '.
         }
+
     };
 }
 const p1 = criaPessoa('João', 'Esteves',82,1.80); // ELE (p1)É O THIS (ONDE ESTA TODAS AS INFORMAÇÕES E FOI QUEM CHAMOU O OBJETO)
@@ -40,4 +49,10 @@ console.log(p2.imc()); //chamando o metodo
 console.log(p2.nomeCompleto); // chamando o getter= metodo que finge ser um atributo (string)
 
 //=============================================================
+//USANDO O SETTER:
+p2.nomeCompleto = 'Otaviana Oliveira Santos';
+console.log(p2.nome); //Otaviana
+console.log(p2.sobrenome); //Oliveira Santos
+
+// O nome Maria Camargo deixou de existir e foi subistituido pelo comando set 
 
