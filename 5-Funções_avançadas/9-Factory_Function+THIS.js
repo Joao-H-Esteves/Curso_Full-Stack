34e3e46d6eb3363a36109472aeb3ed033d3842bb
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-06-21 10:36:35 -0300

    factory functions

--- 5-Funções_avançadas/9-Factory_Function+THIS.js
+++ 5-Funções_avançadas/9-Factory_Function+THIS.js
@@ -1,20 +1,43 @@
-// FUNÇÕES QUE RETORNAM OBJETOS
+// FACTORY = FUNÇÕES QUE RETORNAM OBJETOS
 
 //QUANDO UMA FUNÇÃO ESTA DENTRO DE UM OBJETO É CHAMADA DE  """MÉTODO"""
 
 //Ex:
-function criaPessoa(nome, sobrenome) {
+function criaPessoa(nome, sobrenome, peso, alt) {
     return {
         nome,
         sobrenome,
-        fala: function (assunto) { // MÉTODO
-            return `${nome} esta falando sobre ${assunto}`;
+
+        fala: function fala (assunto) { // MÉTODO (nome primeiro: function anonima depois)
+            return `${nome}, ${this.cargo}, esta falando sobre ${assunto}`;
+            //PARA ACESSSAR CHAVES QUE NÃO FORAM DECLARADAS NOS PARAMETROS USAMOS O 'THIS.' QUE SE REFERE AO P1
+        },
+
+        cargo: 'aluno',
+        peso,
+        altura: alt,
+
+        imc() { // Forma curta de criar um MÉTODO
+            const calcIndicie = this.peso / (this.altura ** 2);
+            return calcIndicie.toFixed(2);
+        },
+            //GETTER: GET
+        get nomeCompleto() { //GET = vai fazer o metodo(funçao) virar um atributo (string), não vai precisar dos '()'
+            return `${this.nome} ${this.sobrenome}`
         }
     };
 }
-const p1 = criaPessoa('João', 'Esteves');
+const p1 = criaPessoa('João', 'Esteves',82,1.80); // ELE (p1)É O THIS (ONDE ESTA TODAS AS INFORMAÇÕES E FOI QUEM CHAMOU O OBJETO)
 
-console.log(p1); //{ nome: 'João', sobrenome: 'Esteves' }
+console.log(p1); //{ nome: 'João', sobrenome: 'Esteves' ,fala: [Function: fala],cargo: 'aluno', peso:82 , alt: 1.80}
+
+console.log(p1.fala('estudos')); //João, aluno, esta falando sobre estudos
+
+const p2 = criaPessoa('Maria', 'Camargo', 80, 1.70);
+
+console.log(p2.fala('provas'));
+console.log(p2.imc()); //chamando o metodo
+console.log(p2.nomeCompleto); // chamando o getter= metodo que finge ser um atributo (string)
 
 //=============================================================
 
