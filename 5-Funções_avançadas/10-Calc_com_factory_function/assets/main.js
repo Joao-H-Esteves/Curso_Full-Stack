df27cda2353b8a8213a3268179ad71265bba6ae6
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-06-22 16:08:52 -0300

    addEventListener no exercicio

--- 5-Funções_avançadas/10-Calc_com_factory_function/assets/main.js
+++ 5-Funções_avançadas/10-Calc_com_factory_function/assets/main.js
@@ -1,10 +1,37 @@
 // VARIAVEIS GLOBAIS
 
-const botoes = document.getElementsByClassName('btnGeneric');
+const calculadora = criaCalculadora();
+calculadora.inicia();
+
+//factory function:
+
+function criaCalculadora() {
+
+    return {
+        result: document.querySelector('.result'), // caixa de texto do total do resultado
+
+        inicia() {
+            this.clicarBotoes();
+            console.log('iniciada');
+        },
+        clicarBotoes() {
+            document.addEventListener('click', function (event) {
+                const apertou = event.target;
+                console.log(apertou);
+                if (apertou.classList.contains('btnGeneric')) {
+                    console.log('apertou o botão');
+                    this.comandoParaTotal(); // caixa de reusltado
+                };
+                
+            })
+        },
+        comandoParaTotal() { // caixa de resultado
+             console.log('teste');
+        }
 
 
+        
 
-let captura = document.addEventListener('click', function (event) {
-    let apertou = event.target;
-    console.log(apertou);
-})
+    };
+
+}
